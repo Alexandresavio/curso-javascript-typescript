6ce437361128ca2c4a24b1276bd7aba8111008d9
savio <alexandresaviosouza@gmail.com>
2024-11-28 07:39:35 -0300

    adicionando arquivos de html e aula005 de JS

---
+++ JS-basico/_aula005/constante.js
@@ -0,0 +1,10 @@
+const n1 = 10
+const n2 = 10
+const resultado = n1 * n2
+
+const resultadoDuplicado = resultado * 2
+
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+
+console.log(resultadoTriplicado)
